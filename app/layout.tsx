02de15806ffd5033c469d83f5e87cbfa7b5a964d
driveronhireadnan <curatogift@gmail.com>
2025-11-17 13:54:59 +0530

    GTM aa

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,17 +49,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* Google Tag Manager - Must be as high as possible in <head> */}
+        {/* Google Tag Manager - MUST be the first element in <head> for verification */}
         <script
           suppressHydrationWarning
           dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-M4L9QDWH');
-            `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M4L9QDWH');`,
           }}
         />
         {/* Prevent iOS auto-detection of phone numbers, emails, etc. which can cause hydration issues */}
