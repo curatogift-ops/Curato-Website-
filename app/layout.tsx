09ecaca7eb828723daa3171d227ac85279288a5a
driveronhireadnan <curatogift@gmail.com>
2025-11-17 16:56:59 +0530

    gtm code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "./globals.css";
 
 import Header from "@/components/layout/Header";
@@ -57,23 +56,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* -------------------------------------------
-           ✔ Google Tag Manager (HEAD)
-        ------------------------------------------- */}
+        {/* Google Tag Manager */}
         <script
+          suppressHydrationWarning
           dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-M4L9QDWH');
-            `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M4L9QDWH');`,
           }}
         />
+        {/* End Google Tag Manager */}
 
         <meta
           name="format-detection"
@@ -114,28 +108,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
 
       <body className="antialiased" suppressHydrationWarning>
-        {/* ✔ DO NOT PUT GTM NOSCRIPT — Search Console will fail anyway */}
-        {/* GTM still works perfectly without noscript */}
-
-        {/* -------------------------------------------
-           ✔ Google Analytics (GA4)
-        ------------------------------------------- */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-LMV7TQ1VZ9"
-          strategy="afterInteractive"
-        />
-        <Script
-          id="ga-init"
-          strategy="afterInteractive"
+        {/* Google Tag Manager (noscript) */}
+        <noscript
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-LMV7TQ1VZ9');
-            `,
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4L9QDWH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
+        {/* End Google Tag Manager (noscript) */}
 
         {/* Layout Components */}
         <HydrationFix />
