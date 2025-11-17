d5da92c8481b353ae5c5cad1c41c10f2d0eecd6a
driveronhireadnan <curatogift@gmail.com>
2025-11-17 13:18:55 +0530

    gtm issue fized

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager - Must be as high as possible in <head> */}
+        <script
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M4L9QDWH');
+            `,
+          }}
+        />
         {/* Prevent iOS auto-detection of phone numbers, emails, etc. which can cause hydration issues */}
         <meta
           name="format-detection"
@@ -146,22 +159,7 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased" suppressHydrationWarning>
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-M4L9QDWH');
-            `,
-          }}
-        />
-        
-        {/* Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript) - Must be immediately after opening <body> tag */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-M4L9QDWH"
